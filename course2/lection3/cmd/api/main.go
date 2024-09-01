7c03b28862b4e348caceb2dc14b4c2b0e2bb96db
lapeko <lapeko88@gmail.com>
2024-09-02 00:00:32 +0200

    small refactoring

--- course2/lection3/cmd/api/main.go
+++ course2/lection3/cmd/api/main.go
@@ -2,23 +2,23 @@ package main
 
 import (
 	"flag"
-	"github.com/lapeko/andersen__programming_in_the_go_language/course2/lection3/internal/api"
+	"github.com/lapeko/andersen__programming_in_the_go_language/course2/lection3/internal/app/api"
 	"log"
 )
 
 var (
-	configType string
-	configPath string
+	configFormat string
+	configPath   string
 )
 
 func init() {
-	flag.StringVar(&configType, "type", "toml", "Type of the config file [env|toml]. Default is \"toml\"")
+	flag.StringVar(&configFormat, "format", "toml", "Type of the config file [env|toml]. Default is \"toml\"")
 	flag.StringVar(&configPath, "path", "", "Path to the app config. Default \"./configs/api.toml\" for type toml and \"./configs/.env\" for type env")
 }
 
 func main() {
 	flag.Parse()
-	config := api.NewConfig(configType, configPath)
+	config := api.NewConfig(configFormat, configPath)
 	a := api.New(config)
 	if err := a.Start(); err != nil {
 		log.Fatalln(err)
