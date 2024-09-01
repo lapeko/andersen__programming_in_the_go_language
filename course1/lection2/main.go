47f07d19800fbff1a26aec0ec1654b382bd6f05f
lapeko <lapeko88@gmail.com>
2024-09-01 19:37:58 +0200

    work with basic types

---
+++ course1/lection2/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"unicode/utf8"
+	"unsafe"
+)
+
+func main() {
+	// numbers
+	num := 3
+	fmt.Printf("Type of num is \"%T\" and it takes %d bytes\n", num, unsafe.Sizeof(num))
+
+	// strings
+	vitali := "Виталя"
+	fmt.Printf("Length of word %s is %d bytes and has %d characters\n", vitali, len(vitali), utf8.RuneCountInString(vitali))
+
+	var l rune = 'ł'
+	fmt.Println(l)
+	var l2 rune = 322
+	fmt.Printf("Character %c has number code %d\n", l2, l2)
+	fmt.Printf("%t", strings.Compare("Вася", "Грыша"))
+
+	if num := 10; num%2 == 0 {
+		fmt.Printf("%d is an even number", num)
+	} else {
+		fmt.Printf("%d is an odd number", num)
+	}
+}
