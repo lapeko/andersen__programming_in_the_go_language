940345fbe4435ba07f41d7bd8a40e8839cdf92de
lapeko <lapeko88@gmail.com>
2024-09-14 21:15:16 +0200

    learn switch/case with fallthrough

--- course1/lection3/main.go
+++ course1/lection3/main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	fallThrough()
+}
+
+func labeledLoop() {
 outer:
 	for i := 0; i < 10; i++ {
 		for j := 0; j < 10; j++ {
@@ -15,5 +19,28 @@ outer:
 			fmt.Print("*")
 		}
 		fmt.Println()
+	}
+}
+
+func fallThrough() {
+	var input int
+
+	fmt.Println("Enter a number")
+
+	_, err := fmt.Scan(&input)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	switch {
+	case input < 100:
+		fmt.Println("Less then 100")
+	case input < 200:
+		fmt.Println("Less then 200")
+		fallthrough
+	case input >= 200:
+		fmt.Println("Higher or equal 200")
 	}
 }
