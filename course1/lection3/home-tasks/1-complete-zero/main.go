2c828aabd060f7b2c90fed188c49a0f4a213368d
lapeko <lapeko88@gmail.com>
2024-09-14 20:59:51 +0200

    lection3 task1

---
+++ course1/lection3/home-tasks/1-complete-zero/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var input string
+
+	for {
+		_, err := fmt.Scanf("%s\n", &input)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if input == "0" {
+			return
+		}
+
+		fmt.Println(input)
+	}
+}
