752e4582f86a30c7aa54e2b1556043d8594fb94a
lapeko <lapeko88@gmail.com>
2024-09-15 22:26:56 +0200

    learn slices

--- course1/lection4/main.go
+++ course1/lection4/main.go
@@ -1,15 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	// simple array
-	var arr1 [2]int
-	arr1[0] = 1
-	arr1[1] = 2
-	fmt.Println(arr1)
+	var arr [2]int
+	arr[0] = 1
+	arr[1] = 2
+	fmt.Println(arr)
 
 	// computed size array
 	arr2 := [...]int{1, 2}
-	fmt.Println(arr1 == arr2)
+	fmt.Println(arr == arr2)
+
+	// slice
+	arr3 := [...]int{3, 4, 5}
+	slice := arr3[:]
+	for idx, _ := range slice {
+		slice[idx]++
+	}
+	fmt.Println(arr3)
+	fmt.Printf("%v.\tSize: %d. Capacity: %d\n", slice, len(slice), cap(slice))
+	slice = append(slice, 7)
+	fmt.Printf("%v.\tSize: %d. Capacity: %d\n", slice, len(slice), cap(slice))
 }
