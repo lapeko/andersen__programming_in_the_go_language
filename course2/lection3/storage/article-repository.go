53afc12bea2f209b2ed31f224e1dbe10f8c4318c
lapeko <lapeko88@gmail.com>
2024-09-16 09:45:50 +0200

    cmplete article repository

--- course2/lection3/storage/article-repository.go
+++ course2/lection3/storage/article-repository.go
@@ -49,7 +49,7 @@ func (a *ArticlesRepository) GetAll() ([]*models.Article, error) {
 	return articles, nil
 }
 
-func (a *ArticlesRepository) GetArticleById(id int) (*models.Article, error) {
+func (a *ArticlesRepository) GetById(id int) (*models.Article, error) {
 	query := fmt.Sprintf("SELECT * from %s WHERE id = $1", articlesTableName)
 
 	article := &models.Article{}
@@ -61,6 +61,58 @@ func (a *ArticlesRepository) GetArticleById(id int) (*models.Article, error) {
 			return nil, nil
 		}
 		return nil, err
+	}
+
+	return article, nil
+}
+
+func (a *ArticlesRepository) Create(article *models.Article) (*models.Article, error) {
+	query := fmt.Sprintf("INSERT INTO %s (title, author_id, content) VALUES ($1, $2, $3) RETURNING id", articlesTableName)
+	err := a.db.QueryRow(query, article.Title, article.AuthorId, article.Content).Scan(&article.Id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return article, nil
+}
+
+func (a *ArticlesRepository) Delete(id int) (bool, error) {
+	query := fmt.Sprintf("DELETE FROM %s WHERE id = $1", articlesTableName)
+	res, err := a.db.Exec(query, id)
+
+	if err != nil {
+		return false, err
+	}
+
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return false, err
+	}
+
+	if rowsAffected == 0 {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+func (a *ArticlesRepository) Update(article *models.Article) (*models.Article, error) {
+	query := fmt.Sprintf("UPDATE %s SET title=$1, author_id=$2, content=$3 WHERE id=$4", articlesTableName)
+	r, err := a.db.Exec(query, article.Title, article.AuthorId, article.Content, article.Id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	rowsAffected, err := r.RowsAffected()
+
+	if err != nil {
+		return nil, err
+	}
+
+	if rowsAffected == 0 {
+		return nil, nil
 	}
 
 	return article, nil
